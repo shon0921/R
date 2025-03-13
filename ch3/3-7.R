1eb5f7a71fb0bf9ab90e1928ca3e236ff7183c6a
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-13 12:22:16 +0900

    변수명 바꾸기

--- ch3/3-7.R
+++ ch3/3-7.R
@@ -2,3 +2,8 @@ DF <- read.csv("example_studentlist.csv", fileEncoding = "CP949")
 colnames(DF)
 colnames(DF)[6] <- "blood"
 DF
+
+OldList <- colnames(DF)
+NewList <- c("na","se","ag","gr","ab","bl","he","we")
+colnames(DF) <- NewList
+DF
