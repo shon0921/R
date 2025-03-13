7423baeb5c29ca9ef94770a74c511437d7daac8c
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-13 12:16:53 +0900

    여러 개 벡터를 만들어 데이터프레임 만들기

---
+++ ch3/3-2.R
@@ -0,0 +1,14 @@
+a1 <- c(5,3,6,3,1)
+a1
+
+a2 <- c("짬뽕","짜장면","짬뽕","짬뽕","짜장면")
+a2
+
+a3 <- c(3.62,5.45,2.54,3.67,7.23)
+a3
+
+DF <- data.frame(a1,a2,a3)
+DF
+
+DF <- data.frame(count=a1, food=a2, meanCount=a3)
+DF
