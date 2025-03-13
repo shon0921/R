2d1e0b695f18d93d0918ffc4303559522ba90f36
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-13 12:11:12 +0900

    패키지(Package)

--- ch2/2-2.R
+++ ch2/2-2.R
@@ -4,26 +4,6 @@ library("rgl")
 
 example(plot3d)
 
-library()
-
-List <- read.csv("example_studentlist.csv", fileEncoding = "CP949")
-List
-
-List <- read.table("studentlist.txt", fileEncoding = "CP949")
-List
-
-List <- read.table("studentlist.txt",fileEncoding = "CP949", header=TRUE)
-List
-
-List <- read.table("studentlist.txt",fileEncoding = "CP949", header=TRUE,sep=";")
-List
-
-install.packages("readxl")
-library("readxl")
-
-DF <- read_excel(path="studentlist.xlsx",sheet="Sheet1",col_names=TRUE)
-DF
-
 install.packages("ggplot2")
 install.packages("ggthemes")
 install.packages("data.table")
@@ -33,3 +13,5 @@ install.packages("plyr")
 install.packages("reshape2")
 install.packages("scales")
 install.packages("stringr")
+
+library()
