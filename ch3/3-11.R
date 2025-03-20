d01ce7d1df8a5f5498a96d18a5e229b1f0204f5e
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-20 10:12:55 +0900

    리스트

---
+++ ch3/3-11.R
@@ -0,0 +1,7 @@
+DF <- read.csv("example_studentlist.csv", fileEncoding = "CP949")
+a <- c(1:20)
+s <- c("파스타", "짬뽕", "순두부찌게", "요거트 아이스크림", "커피")
+L <- c(T,F,F,T,T,T)
+
+List <- list(DF, a,s,L	)
+List
